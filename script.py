f7f24da9dbef92d171d188c1057b065fea53ca22
EHubert <eric.c.hubert@gmail.com>
2020-02-18 17:51:38 -0500

    Initial Commit

--- script.py
+++ script.py
@@ -1,18 +1,7 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    test = "Test"
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
+test = "Test"
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
